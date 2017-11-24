4027400a533f6f9ec059e7624f55ac08facf951b
hchiam <hchiam@gmail.com>
2017-11-23 22:00:56 -0500

    avoid error if no letters found

--- webScraper.py
+++ webScraper.py
@@ -1,13 +1,13 @@
 # The MIT License (MIT)
-# 
+#
 # Copyright (c) 2016 Howard Chiam
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-# 
+#
 # --------------
 
 # Reference:  http://web.stanford.edu/~zlotnick/TextAsData/Web_Scraping_with_Beautiful_Soup.html
@@ -30,17 +30,18 @@ print type(soup)
 letters = soup.find_all("div", class_="ec_statements")
 print "\ntype(letters) = "
 print type(letters)
-print "\nletters[0] = "
-print letters[0]
-print "\nletters[0].a[\"href\"] = \n\t\t(using the \"a\" tag)"
-print letters[0].a["href"]
-print "\n\nletters[0].find(id=\"legalert_date\") = \n\t\t(finding div tags with id \"legalert_date\")"
-print letters[0].find(id="legalert_date")
-print "\n\nletters[0].find(id=\"legalert_title\") = \n\t\t(finding div tags with id \"legalert_title\")"
-print letters[0].find(id="legalert_title")
-print "\n\nletters[0].find(id=\"legalert_title\").get_text() = \n\t\t(finding text of div tags with id \"legalert_title\")"
-print letters[0].find(id="legalert_title").get_text()
-print "\n"
+if letters:
+    print "\nletters[0] = "
+    print letters[0]
+    print "\nletters[0].a[\"href\"] = \n\t\t(using the \"a\" tag)"
+    print letters[0].a["href"]
+    print "\n\nletters[0].find(id=\"legalert_date\") = \n\t\t(finding div tags with id \"legalert_date\")"
+    print letters[0].find(id="legalert_date")
+    print "\n\nletters[0].find(id=\"legalert_title\") = \n\t\t(finding div tags with id \"legalert_title\")"
+    print letters[0].find(id="legalert_title")
+    print "\n\nletters[0].find(id=\"legalert_title\").get_text() = \n\t\t(finding text of div tags with id \"legalert_title\")"
+    print letters[0].find(id="legalert_title").get_text()
+    print "\n"
 
 #_________________________________________________
 # Code I made to get the Greek translation of the word "Lojban" from the website Wiktionary:
